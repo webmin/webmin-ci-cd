de52f1c2b7c68133fbc29843685836839be946ac
Ilia Ross <ilia@ross.gdn>
2025-12-21 00:09:50 +0200

    Fix to shared secret to trust `X-Auth-IP` from repo server

--- build/mod-auth-check-external.bash
+++ build/mod-auth-check-external.bash
@@ -90,6 +90,7 @@ printf 'machine %s login %s password %s\n' "${host}" "${login}" "${password}" > 
 status="$(curl -sS -o /dev/null -w '%{http_code}' \
                --netrc-file "${netrc}" \
                -H "X-Auth-IP: ${REMOTE_ADDR:-}" \
+               -H "X-Auth-Secret: SECRET" \
                --connect-timeout 3 --max-time 8 \
                "${ENDPOINT}" || true)"
 

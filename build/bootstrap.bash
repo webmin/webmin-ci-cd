756492c83e756bcbf39044adbff86a800a1e68f3
Ilia Ross <ilia@ross.gdn>
2026-02-10 20:16:17 +0200

    2.3.5

--- build/bootstrap.bash
+++ build/bootstrap.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091
 # bootstrap.bash (https://github.com/webmin/webmin-ci-cd)
-# Version 2.3.4
+# Version 2.3.5
 # Copyright Ilia Ross <ilia@webmin.dev>
 # Licensed under the MIT License
 #
